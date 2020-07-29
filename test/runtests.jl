f4764fff106016980a385d95b6a249c811bd88f5
Rik <rik@huijzer.xyz>
2020-07-29 19:43:58 +0200

    Add RCall

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using Test
 using GenDoc
 
+import RCall
+
+function test_rcall()
+    lsd = RCall.reval("ls(\"package:datasets\")")
+    @test isa(lsd, RCall.RObject{RCall.StrSxp})
+    @test length(lsd) > 50
+end
+
+test_rcall()
 @test hello("Julia") == "Hello, Julia"
 @test domath(2.0) ≈ 7.0
