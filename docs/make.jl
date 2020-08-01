731864536b0ddb696c0a553c6450e608ac08593e
Rik <rik@huijzer.xyz>
2020-08-01 15:40:07 +0200

    Document disable prettyurls locally

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,12 @@ using GenDoc
 
 include("../gendocdocs/gettingstarted.jl")
 
-makedocs(sitename = "GenDoc.jl",
+makedocs(
+    sitename = "GenDoc.jl",
     pages = [
         "Getting started" => write_gettingstarted()
-    ]
+    ],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true")
 )
 
 deploydocs(repo = "github.com/rikhuijzer/GenDoc.jl.git")
