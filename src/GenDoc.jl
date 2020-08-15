6d8ef7be86b1c15825d455def97aa9c1a96659ef
Rik <rik@huijzer.xyz>
2020-08-15 19:09:36 +0200

    Cut setting optlevel; doesnt help on site build

--- src/GenDoc.jl
+++ src/GenDoc.jl
@@ -1,9 +1,5 @@
 module GenDoc
 
-if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
-    @eval Base.Experimental.@optlevel 1
-end
-
 include("utils.jl")
 include("md.jl")
 
