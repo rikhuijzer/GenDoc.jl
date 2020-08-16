20a1fcb314c0a87c0c50c6f960973437ed9ce631
Rik <rik@huijzer.xyz>
2020-08-16 18:28:11 +0200

    Add is_ci

--- src/utils.jl
+++ src/utils.jl
@@ -1,17 +1,20 @@
 export
-    is_gitlab_ci
+    is_ci
 
-"""
-    is_gitlab_ci()::Bool
-
-Return whether this project is being executed on GitLab's CI.
-"""
+is_github_ci()::Bool = ENV["CI"] == true
 is_gitlab_ci()::Bool = "CI_PIPELINE_SOURCE" in keys(ENV)
+
+"""
+    is_ci()::Bool
+
+Return whether this project is being executed on GitHub's or GitLab's CI.
+"""
+is_ci()::Bool = is_gitlab_ci() || is_github_ci()
 
 """
     project_root()::String
 
-Return the root directory of the project.
+Return the root directory of GenDoc.jl.
 """
 function project_root()::String
     dirparent(dir) = splitdir(endswith(dir, '/') ? dir[1:end-1] : dir)[1]
